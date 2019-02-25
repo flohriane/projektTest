098d96ac25ae4ba2b52930941a9dd07c6ec2e38f
flohriane <flohr.christiane@gmail.com>
2019-02-25 12:18:46 +0100

    Initial commit

---
+++ projektTest/ProjektClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektTest
+{
+    public class ProjektClass
+    {
+        public static List<int> Start_Zahlen_testen(int untergrenze, int obergrenze)
+        {
+            var zahlen = Zahlen_testen(untergrenze, obergrenze);
+            return zahlen;
+        }
+        internal static List<int> Zahlen_testen(int ug, int og)
+        {
+            var zahlen = new List<int>();
+            for (int z = ug; z <= og; z++)
+            {
+                zahlen.Add(z);
+            }
+            return zahlen;
+        }
+    }
+}
---
+++ testsFürProjektTest/Tests1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using projektTest;
+
+namespace testsFürProjektTest
+{
+    [TestFixture]
+    public class Tests1
+    {
+        [Test, Category ("Akzeptanztest")]
+        public void Zahlen_zwischen_UG_OG()
+        {
+            var ergebnis = ProjektClass.Start_Zahlen_testen(3, 5);
+            Assert.AreEqual(new[] { 3, 4, 5 }, ergebnis.ToArray());
+        }
+        [Test, Category("Gerüsttest")]
+        public void Test_Zahlen_testen()
+        {
+            var ergebnis = ProjektClass.Zahlen_testen(0, 23);
+            Assert.AreEqual(24, ergebnis.Count());
+        }
+    }
+}
